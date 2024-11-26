675b3abfd771a9560aafd634825a78108638057e
Howard <iamxyzgos5933@gmail.com>
2024-11-26 17:05:09 +0800

    new project

---
+++ src/main/java/com/devpather/buyla_backend/BuylaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devpather.buyla_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuylaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuylaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpather/buyla_backend/BuylaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpather.buyla_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuylaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
